b67a7031860b6670ca1773341d8a0663e4b7409e
riverajefer <jefersonpatino@yahoo.es>
2017-01-19 16:59:51 -0500

    Work 19-01

--- app/src/main/java/jeffer/com/firebase2test/MainActivity.java
+++ app/src/main/java/jeffer/com/firebase2test/MainActivity.java
@@ -2,12 +2,14 @@ package jeffer.com.firebase2test;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.CompoundButton;
+import android.widget.Switch;
 
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends AppCompatActivity {
-
+    Switch mySwitch;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -15,5 +17,19 @@ public class MainActivity extends AppCompatActivity {
         FirebaseDatabase database = FirebaseDatabase.getInstance();
         DatabaseReference myRef = database.getReference("message");
         myRef.setValue("Hello, World!");
+
+        mySwitch = (Switch)findViewById(R.id.mySwitch);
+
+        mySwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked) {
+                    mySwitch.setText("Luz Sala Encendida");
+
+                } else {
+                    mySwitch.setText("Luz Sala Apagado");
+                }
+            }
+        });
     }
 }
