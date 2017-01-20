bf97a19bbd4380ff1ed349f4c5e7509586e4bd5b
riverajefer <jefersonpatino@yahoo.es>
2017-01-20 17:02:01 -0500

    Work 20-01

--- app/src/main/java/jeffer/com/firebase2test/MainActivity.java
+++ app/src/main/java/jeffer/com/firebase2test/MainActivity.java
@@ -10,26 +10,44 @@ import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends AppCompatActivity {
     Switch mySwitch;
+    Switch mySwitch2;
+    DatabaseReference myRefLed1, myRefLed2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
         FirebaseDatabase database = FirebaseDatabase.getInstance();
-        DatabaseReference myRef = database.getReference("message");
-        myRef.setValue("Hello, World!");
+
+        myRefLed1 = database.getReference("home/led1");
+        myRefLed2 = database.getReference("home/led2");
 
         mySwitch = (Switch)findViewById(R.id.mySwitch);
+        mySwitch2 = (Switch)findViewById(R.id.mySwitch2);
 
         mySwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                if (isChecked) {
-                    mySwitch.setText("Luz Sala Encendida");
+            myRefLed1.setValue(isChecked);
+            if (isChecked) {
+                mySwitch.setText("Led 1 On");
+            } else {
+                mySwitch.setText("Led 1 Off");
+            }
+            }
+        });
 
+        mySwitch2.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                myRefLed2.setValue(isChecked);
+                if (isChecked) {
+                    mySwitch2.setText("Led 2 On");
                 } else {
-                    mySwitch.setText("Luz Sala Apagado");
+                    mySwitch2.setText("Led 2 Off");
                 }
             }
         });
+
     }
 }
